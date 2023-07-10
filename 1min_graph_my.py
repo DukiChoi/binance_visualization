daf5d3a8d2f45a7dfa84fac72830665ea58ddc43
DukiChoi <cbcc12345@hanyang.ac.kr>
2023-07-10 14:23:01 +0900

    Changed Text size

--- 1min_graph_my.py
+++ 1min_graph_my.py
@@ -109,7 +109,7 @@ class MyWindow(QMainWindow):
         self.text = QPlainTextEdit(self)
         self.text.isReadOnly()
         self.text.move(10, 10)
-        self.text.resize(500, 120)
+        self.text.resize(500, 100)
         style_sheet = "QPlainTextEdit { font-size: 40px;}"
         self.text.setStyleSheet(style_sheet)
 
